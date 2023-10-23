a7e32464f3017bb2aa3e9e2d692959f87f85e32c
Bootcamp74 <148359746+Bootcamp74@users.noreply.github.com>
2023-10-22 22:55:48 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 
-// Fetch response
+// Fetch response for weather
 
 
-requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=51.4875167&lon=-0.1687007&appid=fc8bffadcdca6a94d021c093eac22797';
 
 fetch(requestUrl)
   .then(function (response) {
