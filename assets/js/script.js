ba8790acf4303e71de3db48ac1c61bcba0ec7b28
Bootcamp74 <148359746+Bootcamp74@users.noreply.github.com>
2023-10-22 19:40:15 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// Fetch response
+
+
+requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
