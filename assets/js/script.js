20ac705d651ed766bf6c679c80d669a17e73600a
Bootcamp74 <148359746+Bootcamp74@users.noreply.github.com>
2023-10-23 13:59:51 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+
 const key = 'fc8bffadcdca6a94d021c093eac22797'
-var city = "chicago"
+var city = document.getElementById("citysearch").value
 var cityTitle= document.getElementById("cityTitle")
 cityTitle.innerHTML = city; 
+var search = document.getElementById("search")
 
 // Fetch request for city name latatude and longitude
 
@@ -18,6 +20,8 @@ fetch(requestUrl0)
     console.log (data[0].lon)
   });
 
+
+
 // Fetch request for weather
 
 requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=51.4875167&lon=-0.1687007&appid='+key;
@@ -29,3 +33,22 @@ fetch(requestUrl)
   .then(function (data) {
     console.log(data);
   });
+
+
+
+// Test for button function
+
+search.onclick = function(hello){
+  var city = document.getElementById("citysearch").value
+
+  // Makes sure the user enters a city in the input field
+  
+  if (document.getElementById("citysearch").value == "" ||document.getElementById("citysearch").value == "Please enter a city name") {
+  document.getElementById("citysearch").value = "Please enter a city name"
+  return}
+
+cityTitle.innerHTML = city;
+};
+ 
+hello();
+
