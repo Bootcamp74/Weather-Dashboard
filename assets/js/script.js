e4be4616d81277952d52182e35d9d87dd32b6d8d
Bootcamp74 <148359746+Bootcamp74@users.noreply.github.com>
2023-10-23 15:23:21 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,51 @@
 
-const key = 'fc8bffadcdca6a94d021c093eac22797'
+
+// Needed to change const key to var key to run in github??
+// Error: "key already defined." Ran fine in Live server
+
+var key = 'fc8bffadcdca6a94d021c093eac22797'
 var city = document.getElementById("citysearch").value
-var cityTitle= document.getElementById("cityTitle")
+// var cityTitle= document.getElementById("cityTitle")
 cityTitle.innerHTML = city; 
 var search = document.getElementById("search")
 
 // Fetch request for city name latatude and longitude
+// function latatude () {
 
-requestUrl0 = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+key;
+// requestUrl0 = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+key;
 
-fetch(requestUrl0)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log (data[0].name)
-    console.log (data[0].lat)
-    console.log (data[0].lon)
-  });
-
+// fetch(requestUrl0)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     console.log (data[0].name)
+//     console.log (data[0].lat)
+//     console.log (data[0].lon)
+//     window.alert(data[0].name)
+//   });
+// return}
 
 
 // Fetch request for weather
 
-requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=51.4875167&lon=-0.1687007&appid='+key;
+// requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=51.4875167&lon=-0.1687007&appid='+key;
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
 
 
 
 // Test for button function
 
-search.onclick = function(hello){
+search.onclick =  function () {
+  
   var city = document.getElementById("citysearch").value
 
   // Makes sure the user enters a city in the input field
@@ -46,9 +53,24 @@ search.onclick = function(hello){
   if (document.getElementById("citysearch").value == "" ||document.getElementById("citysearch").value == "Please enter a city name") {
   document.getElementById("citysearch").value = "Please enter a city name"
   return}
+  else {
 
-cityTitle.innerHTML = city;
-};
+
+      requestUrl0 = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+key;
+      
+      fetch(requestUrl0)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          console.log (data[0].name)
+          console.log (data[0].lat)
+          console.log (data[0].lon)
+          window.alert(data[0].name)
+          document.getElementById("cityTitle").innerHTML = data[0].name
+        });
+      return}
+    
+  };
  
-hello();
-
